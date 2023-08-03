7cb3bbe06086c24260da309868a1c9c6f97010f3
Khadija Sikander <khadijasikander02@gmail.com>
2023-08-04 00:28:43 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+// //Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person='Maulana Rumi';
+let message='"Do not feel lonely, the entire universe is inside you."';
+
+console.log(famous_person, "Said : " + message);
